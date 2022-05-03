53a233758ad75f0d92da52b84a6259b3eb851392
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-03 18:02:25 +0300

    Add files via upload

---
+++ C# Basics/First Steps In Coding - Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace InchesToCm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double cm = inch * 2.54;
+            Console.WriteLine(cm);
+
+        }
+    }
+}
