7f494c350917efa7f354ab19e0a057f90f3dbc64
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-03 17:39:07 +0300

    Add files via upload

---
+++ 05. Greeting by Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace GreetingsByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+
+        }
+    }
+}
