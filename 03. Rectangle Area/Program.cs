52757679c16e26e49e62cd616791b41a48d4eb6b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-03 17:36:25 +0300

    Add files via upload

---
+++ 03. Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * b);
+        }
+    }
+}
