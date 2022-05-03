2bb473842ba14dba94803d6e0ff5a768f4ea31a5
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-03 17:48:04 +0300

    Add files via upload

---
+++ 08. Pet Shop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+            double finalprice = dogFood * 2.50 + catFood * 4;
+            Console.WriteLine($"{finalprice} lv.");
+
+        }
+    }
+}
