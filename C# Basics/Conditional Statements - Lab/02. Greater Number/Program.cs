6cc18784bab2f3ed2d584c967b87155f57b48c65
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-05 23:13:41 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements - Lab/02. Greater Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Conditions.GradeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberone = int.Parse(Console.ReadLine());
+            int numbertwo = int.Parse(Console.ReadLine());
+            if (numberone > numbertwo)
+            {
+                Console.WriteLine(numberone);
+            }
+            else
+            {
+                Console.WriteLine(numbertwo);
+            }
+        }
+    }
+}
