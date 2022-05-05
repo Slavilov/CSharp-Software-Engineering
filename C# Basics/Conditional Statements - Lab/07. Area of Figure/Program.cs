feb4d915dca74e82f6a3eb63dab3da2fd67a66ef
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-05 23:15:47 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements - Lab/07. Area of Figure/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Conditions.GradeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            if (figure == "square")
+            {
+                Console.WriteLine("Molq vuvedete stranata na kvadrata: ");
+                double side = double.Parse(Console.ReadLine());
+                double area = (side * side);
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                Console.WriteLine("Molq vuvedete dvete strani na pravougulnika:");
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                double areaR = sideA * sideB;
+                Console.WriteLine($"{areaR:F3}");
+            }
+            else if (figure == "circle")
+            {
+                Console.WriteLine("Molq vuvedete radiusa na kruga: ");
+                double radius = double.Parse(Console.ReadLine());
+                double areaC = Math.PI * radius * radius;
+                Console.WriteLine($"{areaC:F3}");
+            }
+            else if (figure == "triangle")
+            {
+                Console.WriteLine("Molq vuvedete duljinata i visochinata na triugulnika: ");
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double areaT = (lenght * width) / 2.0;
+                Console.WriteLine($"{areaT:F3}");
+            }
+        }
+    }
+}
+
