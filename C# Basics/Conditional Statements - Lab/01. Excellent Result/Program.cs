57f84717a7cbb2cdddc1865eb97a4a9cf39a0151
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-05 23:12:50 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements - Lab/01. Excellent Result/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Conditions.GradeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
