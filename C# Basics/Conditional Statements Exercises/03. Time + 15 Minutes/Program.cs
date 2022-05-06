507e89ea7c19456724d46594faacde0e6b0fcde4
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-06 22:56:42 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements Exercises/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Conditions.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int hoursToMins = hours * 60;
+
+            int sumOfAll = hoursToMins + minutes;
+            int sumPlus = sumOfAll + 15;
+
+            int newHours = sumPlus / 60;
+            int newMinutes = sumPlus % 60;
+
+            if (newHours >= 24)
+            {
+                newHours = 0;
+                if (newMinutes < 10)
+                {
+                    Console.WriteLine($"{newHours}:0{newMinutes}");
+                }
+                else
+                {
+                    Console.WriteLine($"{newHours}:{newMinutes}");
+                }
+            }
+            else if (newMinutes < 10)
+            {
+                Console.WriteLine($"{newHours}:0{newMinutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{newHours}:{newMinutes}");
+            }
+        }
+    }
+}
