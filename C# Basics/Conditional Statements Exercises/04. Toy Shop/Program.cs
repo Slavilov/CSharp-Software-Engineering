c925bf1498ee497ee523b5676cb958dbc1c51297
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-06 22:56:58 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements Exercises/04. Toy Shop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Conditions.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceOfVacation = double.Parse(Console.ReadLine());
+            double discount = 0;
+
+            int puzzles = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int teddyBears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            int sumOfToys = puzzles + dolls + teddyBears + minions + trucks;
+
+            double sumOfToysPrice = (puzzles * 2.60 + dolls * 3.00 + teddyBears * 4.10 + minions * 8.20 + trucks * 2.00);
+
+            if (sumOfToys >= 50)
+            {
+                sumOfToysPrice = sumOfToysPrice - (sumOfToysPrice * 0.25);
+            }
+
+            sumOfToysPrice = sumOfToysPrice - (sumOfToysPrice * 0.10);
+
+            if (sumOfToysPrice >= priceOfVacation)
+            {
+                Console.WriteLine($"Yes! {(sumOfToysPrice - priceOfVacation):F2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {(priceOfVacation - sumOfToysPrice):F2} lv needed.");
+            }
+        }
+
+    }
+}
