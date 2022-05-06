ca04c6ac80482b563c61f8b721cc14f012416892
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-06 22:57:10 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements Exercises/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Conditions.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int statisti = int.Parse(Console.ReadLine());
+            double outfitPrice = double.Parse(Console.ReadLine());
+            double finalOutfitPrice = statisti * outfitPrice;
+
+            double decor = money * 0.10;
+            if (statisti > 150)
+            {
+                finalOutfitPrice = finalOutfitPrice - (finalOutfitPrice * 0.10);
+
+            }
+
+            if (decor + finalOutfitPrice > money)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {((decor + finalOutfitPrice) - money):F2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {(money - (decor + finalOutfitPrice)):F2} leva left.");
+            }
+        }
+
+    }
+}
