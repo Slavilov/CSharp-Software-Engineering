8885fc7873d657de8237e6546aa331223ac15c0a
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-06 22:56:08 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements Exercises/01. Sum Seconds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditions.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int SecondTime = int.Parse(Console.ReadLine());
+            int ThirdTime = int.Parse(Console.ReadLine());
+            int sum = firstTime + SecondTime + ThirdTime;
+            double one = sum / 60;
+            double two = sum % 60;
+            if (two < 10)
+            {
+                Console.WriteLine($"{one}:0{two}");
+            }
+            else
+            {
+                Console.WriteLine($"{one}:{two}");
+            }
+        }
+    }
+}
