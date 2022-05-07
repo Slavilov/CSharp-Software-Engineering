4577b784368255dd309ce717de2e09b46a8dd0f7
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-07 22:41:23 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements Advanced - Lab/12. Trade Commissions/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace TradeCommissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double volumeOfSales = double.Parse(Console.ReadLine());
+            if (volumeOfSales < 0)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            double comission = 0;
+
+            switch (city)
+            {
+                case "Sofia":
+                    if (volumeOfSales < 0)
+                    {
+                        Console.WriteLine("error");
+                        return;
+                    }
+                    else if (volumeOfSales > 0 && volumeOfSales <= 500)
+                    {
+                        comission = volumeOfSales * 0.05;
+                    }
+                    else if (volumeOfSales > 500 && volumeOfSales <= 1000)
+                    {
+                        comission = volumeOfSales * 0.07;
+                    }
+                    else if (volumeOfSales > 1000 && volumeOfSales < 10000)
+                    {
+                        comission = volumeOfSales * 0.08;
+                    }
+                    else if (volumeOfSales > 10000)
+                    {
+                        comission = volumeOfSales * 0.12;
+                    }
+                    Console.WriteLine($"{comission:f2}");
+                    break;
+
+                case "Varna":
+                    if (volumeOfSales < 0)
+                    {
+                        Console.WriteLine("error");
+                        return;
+                    }
+                    else if (volumeOfSales > 0 && volumeOfSales <= 500)
+                    {
+                        comission = volumeOfSales * 0.045;
+                    }
+                    else if (volumeOfSales > 500 && volumeOfSales <= 1000)
+                    {
+                        comission = volumeOfSales * 0.075;
+                    }
+                    else if (volumeOfSales > 1000 && volumeOfSales < 10000)
+                    {
+                        comission = volumeOfSales * 0.10;
+                    }
+                    else if (volumeOfSales > 10000)
+                    {
+                        comission = volumeOfSales * 0.13;
+                    }
+                    Console.WriteLine($"{comission:f2}");
+                    break;
+
+                case "Plovdiv":
+                    if (volumeOfSales < 0)
+                    {
+                        Console.WriteLine("error");
+                        return;
+                    }
+                    else if (volumeOfSales > 0 && volumeOfSales <= 500)
+                    {
+                        comission = volumeOfSales * 0.055;
+                    }
+                    else if (volumeOfSales > 500 && volumeOfSales <= 1000)
+                    {
+                        comission = volumeOfSales * 0.08;
+                    }
+                    else if (volumeOfSales > 1000 && volumeOfSales < 10000)
+                    {
+                        comission = volumeOfSales * 0.12;
+                    }
+                    else if (volumeOfSales > 10000)
+                    {
+                        comission = volumeOfSales * 0.145;
+                    }
+                    Console.WriteLine($"{comission:f2}");
+                    break;
+                default:
+
+                    Console.WriteLine("error");
+                    break;
+            }
+        }
+    }
+}
