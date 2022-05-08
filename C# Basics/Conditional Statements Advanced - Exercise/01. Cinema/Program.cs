6e9f1b2289241143f21a6610b49693c7a97c08e1
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-08 23:25:02 +0300

    Add files via upload

---
+++ C# Basics/Conditional Statements Advanced - Exercise/01. Cinema/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int row = int.Parse(Console.ReadLine());
+            int column = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            switch (projection)
+            {
+                case "Premiere":
+                    sum = 12.00 * (row * column);
+                    break;
+
+                case "Normal":
+                    sum = 7.50 * (row * column);
+                    break;
+
+                case "Discount":
+                    sum = 5.00 * (row * column);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+            }
+            if (sum != 0)
+            {
+                Console.WriteLine($"{sum:f2} leva");
+            }
+        }
+    }
+}
