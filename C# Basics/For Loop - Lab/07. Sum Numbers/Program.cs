57f4f52d1ab4aeab8219fea5e6e11ee8c994e54c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-09 23:22:34 +0300

    Add files via upload

---
+++ C# Basics/For Loop - Lab/07. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                int chislo = int.Parse(Console.ReadLine());
+                sum += chislo;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
