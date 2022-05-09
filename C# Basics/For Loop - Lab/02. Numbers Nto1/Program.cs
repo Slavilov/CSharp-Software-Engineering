c813e89a48b016132973552ab3c8f6d5102dea9b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-09 23:07:22 +0300

    Add files via upload

---
+++ C# Basics/For Loop - Lab/02. Numbers Nto1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersNto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = int.Parse(Console.ReadLine()); number > 0; number--)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
