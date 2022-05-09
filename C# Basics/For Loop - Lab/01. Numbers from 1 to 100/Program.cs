1f740ba2c482c7da1f3a39ddd3314f1b50d94664
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-09 23:07:02 +0300

    Add files via upload

---
+++ C# Basics/For Loop - Lab/01. Numbers from 1 to 100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersFrom1To100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number < 101; number++)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
