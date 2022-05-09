549b9dd83f5c63859deacb77ace2fa510731f788
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-09 23:23:42 +0300

    Add files via upload

---
+++ C# Basics/For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _09_10_Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sumOfEven = 0;
+            double sumOfOdd = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sumOfEven = sumOfEven + number;
+                }
+                if (i % 2 != 0)
+                {
+                    sumOfOdd = sumOfOdd + number;
+                }
+            }
+            if (sumOfEven == sumOfOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumOfEven}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sumOfOdd - sumOfEven)}");
+            }
+        }
+    }
+}
