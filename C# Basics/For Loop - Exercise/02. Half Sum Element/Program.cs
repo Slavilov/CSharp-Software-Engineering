ba5870b17ee9f415eaf69bb0e3b38611863ea5c9
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-10 23:53:39 +0300

    Add files via upload

---
+++ C# Basics/For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            double maxN = 0;
+            double number = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                number = double.Parse(Console.ReadLine());
+                sum = sum + number;
+
+                if (number > maxN)
+                {
+                    maxN = number;
+                }
+            }
+            if (maxN == (sum - maxN))
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = " + maxN);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxN - (sum - maxN))}");
+            }
+        }
+    }
+}
