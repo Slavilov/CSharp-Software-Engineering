ab2d04bcf3bfd6723876752571274296c7cdacf4
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-11 22:49:51 +0300

    Add files via upload

---
+++ C# Basics/For Loop - Exercise/05. Salary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            string tabName = "";
+            int fine = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                tabName = Console.ReadLine();
+
+                switch (tabName)
+                {
+                    case "Facebook":
+                        fine = fine + 150;
+                        break;
+
+                    case "Instagram":
+                        fine = fine + 100;
+
+                        break;
+
+                    case "Reddit":
+                        fine = fine + 50;
+                        break;
+                }
+                if (salary <= fine)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    i = n;
+                }
+            }
+            if (salary > fine)
+            {
+                Console.WriteLine($"{salary - fine}");
+            }
+
+        }
+    }
+}
