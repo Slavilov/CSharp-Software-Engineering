313355da698b9082fc38af455d8b3ea414a50529
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-12 23:14:39 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Lab/02. Password/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            string passwordForLogin = Console.ReadLine();
+
+            if (passwordForLogin == password)
+            {
+                Console.WriteLine($"Welcome {username}!");
+            }
+
+            while (passwordForLogin != password)
+            {
+                passwordForLogin = Console.ReadLine();
+                if (passwordForLogin == password)
+                {
+                    Console.WriteLine($"Welcome {username}!");
+                }
+            }
+        }
+    }
+}
