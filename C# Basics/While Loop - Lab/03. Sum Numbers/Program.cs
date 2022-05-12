e72f6e7e8b07a85d218d4439b6b160ac90981ec2
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-12 23:14:52 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sumNumbers = 0;
+            int numberTwo = 0;
+
+            while (number > sumNumbers)
+            {
+                numberTwo = int.Parse(Console.ReadLine());
+                sumNumbers = sumNumbers + numberTwo;
+            }
+
+            Console.WriteLine(sumNumbers);
+        }
+    }
+}
