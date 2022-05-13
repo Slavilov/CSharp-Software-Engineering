7184942f670238b588d751c211869cb0aef9a6ed
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-13 23:11:30 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int maxNumber = int.MinValue;
+            int number = 0;
+
+            while (input != "Stop")
+            {
+                number = int.Parse(input);
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
