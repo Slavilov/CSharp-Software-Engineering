0d87ffe49d5abbe487580b407694149a61ed8460
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-13 23:11:59 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Lab/08. Graduation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int gradeCounter = 0;
+            double grade = 0;
+            double averageGrade = 0;
+            double sumOfGrades = 0;
+
+            while (gradeCounter < 12)
+            {
+                gradeCounter += 1;
+                grade = double.Parse(Console.ReadLine());
+                sumOfGrades = sumOfGrades + grade;
+                if (grade < 4)
+                {
+                    Console.WriteLine($"{name} has been excluded at {gradeCounter} grade");
+                    return;
+                }
+            }
+            averageGrade = sumOfGrades / 12;
+            Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+        }
+    }
+}
