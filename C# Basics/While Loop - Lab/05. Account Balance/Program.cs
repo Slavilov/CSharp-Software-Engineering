81e181321cc91ff510894c68a1eee7b3ed100207
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-13 23:11:15 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string deposit = Console.ReadLine();
+            double sum = 0;
+            double add = 0;
+
+            while (deposit != "NoMoreMoney")
+            {
+                add = double.Parse(deposit);
+
+                if (add < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {add:F2}");
+                sum += add;
+                deposit = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Total: {sum:F2}");
+        }
+    }
+}
