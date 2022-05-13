d53495ccbac12313efd47f1b506868027b50c587
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-13 23:11:47 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int number = 0;
+            int minNumber = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                number = int.Parse(input);
+
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
