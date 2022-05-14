7d1f124eeae184aef1e9b28f8203e0b5276afc77
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-14 23:59:25 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteBook = Console.ReadLine();
+            string bookSearch = "";
+            int counter = 0;
+
+            while (bookSearch != favoriteBook && bookSearch != "No More Books")
+            {
+                bookSearch = Console.ReadLine();
+                if (bookSearch == "No More Books")
+                {
+                    break;
+                }
+                if (bookSearch == favoriteBook)
+                {
+                    break;
+                }
+                counter++;
+            }
+            if (bookSearch == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+            else
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+        }
+    }
+}
