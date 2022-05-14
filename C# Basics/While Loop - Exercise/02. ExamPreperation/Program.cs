58ed618b64e6a8fa646974a8e234ab35c5f92c94
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-14 23:59:37 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Exercise/02. ExamPreperation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ExamPreperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxBadGrades = int.Parse(Console.ReadLine());
+            string zadacha = "";
+            int grade = 0;
+            int counter = 0;
+            int counterBadGrade = 0;
+            double counterGrade = 0;
+            string counterZadacha = "";
+
+            while (true)
+            {
+                counterZadacha = zadacha;
+                zadacha = Console.ReadLine();
+
+                if (zadacha == "Enough")
+                {
+                    break;
+                }
+
+                grade = int.Parse(Console.ReadLine());
+                counter++;
+                counterGrade = grade + counterGrade;
+
+                if (grade <= 4)
+                {
+                    counterBadGrade++;
+                }
+                if (counterBadGrade >= maxBadGrades)
+                {
+                    break;
+                }
+            }
+            if (zadacha == "Enough")
+            {
+                Console.WriteLine($"Average score: {(counterGrade / counter):f2}");
+                Console.WriteLine($"Number of problems: {counter}");
+                Console.WriteLine($"Last problem: {counterZadacha}");
+            }
+            else
+            {
+                Console.WriteLine($"You need a break, {counterBadGrade} poor grades.");
+            }
+
+        }
+    }
+}
