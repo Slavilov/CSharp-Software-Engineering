27ab309cbef657714a8134b4398de4fdd6b9d620
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-15 22:59:56 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Exercise/5. Coins/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumToReturn = Math.Floor(100 * double.Parse(Console.ReadLine()));
+            int counter = 0;
+
+            while (sumToReturn > 0)
+            {
+                while (sumToReturn >= 200)
+                {
+                    sumToReturn = sumToReturn - 200;
+                    counter++;
+                }
+
+                while (sumToReturn >= 100)
+                {
+                    sumToReturn = sumToReturn - 100;
+                    counter++;
+                }
+
+                while (sumToReturn >= 50)
+                {
+                    sumToReturn = sumToReturn - 50;
+                    counter++;
+                }
+
+                while (sumToReturn >= 20)
+                {
+                    sumToReturn = sumToReturn - 20;
+                    counter++;
+                }
+
+                while (sumToReturn >= 10)
+                {
+                    sumToReturn = sumToReturn - 10;
+                    counter++;
+                }
+
+                while (sumToReturn >= 5)
+                {
+                    sumToReturn = sumToReturn - 5;
+                    counter++;
+                }
+
+                while (sumToReturn >= 2)
+                {
+                    sumToReturn = sumToReturn - 2;
+                    counter++;
+                }
+
+                while (sumToReturn >= 1)
+                {
+                    sumToReturn = sumToReturn - 1;
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
