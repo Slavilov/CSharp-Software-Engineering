e56103592da57fd5f84ec7557ff8110fa76fd926
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-15 23:00:09 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int lice = height * width;
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "STOP")
+                {
+                    Console.WriteLine($"{lice} pieces are left.");
+                    break;
+                }
+
+                int pieceOfCakeTaken = int.Parse(input);
+                lice = lice - pieceOfCakeTaken;
+                if (lice <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(lice)} pieces more.");
+                    break;
+                }
+
+            }
+        }
+    }
+}
