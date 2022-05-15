dfe85ed2e1a647ad11aad022e960ad6a1647b14d
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-15 22:59:42 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Exercise/4. Walking/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goal = 10000;
+            double sum = 0;
+            double inputConvert = 0;
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+
+                if (input == "Going home")
+                {
+                    inputConvert = double.Parse(Console.ReadLine());
+                    sum = sum + inputConvert;
+
+                    if (sum < goal)
+                    {
+                        Console.WriteLine($"{goal - sum} more steps to reach goal.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Goal reached! Good job!");
+                        Console.WriteLine($"{sum - goal} steps over the goal!");
+                    }
+
+                    break;
+                }
+
+                inputConvert = double.Parse(input);
+                sum = sum + inputConvert;
+
+                if (sum >= goal)
+                {
+                    Console.WriteLine($"Goal reached! Good job!");
+                    Console.WriteLine($"{sum - goal} steps over the goal!");
+                    break;
+                }
+            }
+        }
+    }
+}
