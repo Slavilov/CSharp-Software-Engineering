2a4665e7699c3543f251793492e2fdb3341d28ce
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-15 23:00:27 +0300

    Add files via upload

---
+++ C# Basics/While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double obem = width * lenght * height;
+            double kashonSum = 0;
+
+            while (true)
+            {
+                if (obem < kashonSum)
+                {
+                    Console.WriteLine($"No more free space! You need {kashonSum - obem} Cubic meters more.");
+                    break;
+                }
+                string kashon = Console.ReadLine();
+
+                if (kashon == "Done")
+                {
+                    if (obem > kashonSum)
+                    {
+                        Console.WriteLine($"{obem - kashonSum} Cubic meters left.");
+                    }
+                    break;
+                }
+
+                double kashonDecimal = double.Parse(kashon);
+                kashonSum += kashonDecimal;
+
+            }
+        }
+    }
+}
