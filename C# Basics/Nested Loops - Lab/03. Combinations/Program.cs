7aa22a0e406ab28103926e459ee799e9ec61b8a0
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-16 23:47:41 +0300

    Add files via upload

---
+++ C# Basics/Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int counter = 0;
+
+            for (i = 0; i <= n; i++)
+            {
+                if (i + j + k == n)
+                {
+                    counter++;
+                }
+
+                for (j = 0; j <= n; j++)
+                {
+                    if (i + j + k == n)
+                    {
+                        counter++;
+                    }
+
+                    for (k = 0; k <= n; k++)
+                    {
+                        if (i + j + k == n)
+                        {
+                            counter++;
+                        }
+
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
