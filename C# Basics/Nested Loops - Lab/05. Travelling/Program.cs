c218f85ccd92321010b390b9ce9fd4454389d2b6
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-17 23:15:17 +0300

    Add files via upload

---
+++ C# Basics/Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Traveling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double savedMoney = 0;
+            double savedMoneyCounter = 0;
+
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                savedMoneyCounter = 0;
+
+                if (destination == "End")
+                {
+                    return;
+                }
+
+                double minimalBudget = double.Parse(Console.ReadLine());
+
+                while (savedMoneyCounter <= minimalBudget)
+                {
+                    savedMoney = double.Parse(Console.ReadLine());
+                    savedMoneyCounter += savedMoney;
+                    if (savedMoneyCounter >= minimalBudget)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+}
