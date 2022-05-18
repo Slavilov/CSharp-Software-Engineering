607f851afa7a770790f56cb5ec82dd877ca78ba7
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-18 23:35:28 +0300

    Add files via upload

---
+++ C# Basics/Nested Loops - Exercise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EqualSumsEvenOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int oddNum;
+            int evenNum;
+            string numToLetter;
+
+            for (int i = n1; i <= n2; i++)
+            {
+                oddNum = 0;
+                evenNum = 0;
+
+                numToLetter = i.ToString();
+
+                for (int j = 0; j < numToLetter.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        evenNum += numToLetter[j];
+                    }
+                    else
+                    {
+                        oddNum += numToLetter[j];
+                    }
+
+                }
+                if (oddNum == evenNum)
+                {
+                    Console.Write(i + " ");
+                }
+
+            }
+        }
+    }
+}
