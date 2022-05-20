8177d9a97a1f74cbffa3429e4e1d478c07a77f60
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-20 23:50:02 +0300

    Add files via upload

---
+++ C# Basics/Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            double grade = 0;
+            double counterGrade = 0;
+            double averageGrade = 0; ;
+            double counterAllGrades = 0;
+            int i = 0;
+            int counter = 0;
+
+            while (input != "Finish")
+            {
+                for (i = 0; i < n; i++)
+                {
+                    grade = double.Parse(Console.ReadLine());
+                    counterGrade += grade;
+                }
+                counter++;
+                counterAllGrades += counterGrade;
+
+                Console.WriteLine($"{input} - {(counterGrade / n):f2}.");
+                counterGrade = 0;
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Student's final assessment is {(counterAllGrades / (n * counter)):f2}.");
+
+        }
+    }
+}
