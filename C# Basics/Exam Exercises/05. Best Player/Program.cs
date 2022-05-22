a061a08af5fd2cf7a11573fa3875381d62a0c4b0
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-22 23:12:33 +0300

    Add files via upload

---
+++ C# Basics/Exam Exercises/05. Best Player/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int goals = 0;
+            string bestPlayer = string.Empty;
+            int bestGoals = 0;
+
+            while (input != "END")
+            {
+                string footballer = input;
+                goals = int.Parse(Console.ReadLine());
+
+                if (goals > bestGoals)
+                {
+                    bestPlayer = input;
+                    bestGoals = goals;
+                }
+                if (goals >= 10)
+                {
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{bestPlayer} is the best player!");
+
+            if (bestGoals >= 3)
+            {
+                Console.WriteLine($"He has scored {bestGoals} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestGoals} goals.");
+            }
+        }
+    }
+}
