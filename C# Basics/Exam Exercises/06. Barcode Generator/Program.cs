a5f2c7c630c2c388ae25db84a34cdaeb109ab322
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-22 23:12:45 +0300

    Add files via upload

---
+++ C# Basics/Exam Exercises/06. Barcode Generator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace barcodeGeneratorNEW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startNum = Console.ReadLine(); string finalNum = Console.ReadLine();
+            for (int a = startNum[0]; a <= finalNum[0]; a++)
+            {
+                for (int b = startNum[1]; b <= finalNum[1]; b++)
+                {
+                    for (int c = startNum[2]; c <= finalNum[2]; c++)
+                    {
+                        for (int d = startNum[3]; d <= finalNum[3]; d++)
+                        {
+                            if ((char)a % 2 == 1 && (char)b % 2 == 1
+                            && (char)c % 2 == 1 && (char)d % 2 == 1)
+                            {
+                                Console.Write($"{(char)a}{(char)b}{(char)c}{(char)d} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
