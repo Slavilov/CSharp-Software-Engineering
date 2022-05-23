b223f6984e6535921c24e35859fbf2681f3c7389
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-23 23:17:01 +0300

    Add files via upload

---
+++ C# Basics/Exam Exercises/05. Care of Puppy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Conditions.GradeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            int foodInGrams = food * 1000;
+            int foodEatenCounter = 0;
+            string input = Console.ReadLine();
+            while (input != "Adopted")
+            {
+                int foodEaten = int.Parse(input);
+                foodEatenCounter += foodEaten;
+                input = Console.ReadLine();
+            }
+
+            if (foodEatenCounter <= foodInGrams)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodInGrams - foodEatenCounter} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {foodEatenCounter - foodInGrams} grams more.");
+            }
+        }
+    }
+}
