842af2ce21d433f891ec7bcaf4542c40bf952b80
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-23 23:14:58 +0300

    Add files via upload

---
+++ C# Basics/Exam Exercises/02. Cat Walking/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Conditions.GradeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesPerDay = int.Parse(Console.ReadLine());
+            int razhodkiDnevno = int.Parse(Console.ReadLine());
+            int caloriesPerDay = int.Parse(Console.ReadLine());
+
+            double sumCaloriesPerMinutes = (minutesPerDay * razhodkiDnevno) * 5;
+
+            if (sumCaloriesPerMinutes >= caloriesPerDay / 2)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {sumCaloriesPerMinutes}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {sumCaloriesPerMinutes}.");
+            }
+
+        }
+    }
+}
