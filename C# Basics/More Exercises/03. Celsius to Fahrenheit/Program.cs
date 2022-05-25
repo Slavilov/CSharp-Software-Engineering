a401d176a543b86d66d2b4a89a232d2916dc1d95
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-25 23:47:37 +0300

    Add files via upload

---
+++ C# Basics/More Exercises/03. Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nums1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degrees = double.Parse(Console.ReadLine());
+            double farenhait = degrees * 1.8 + 32;
+            Console.WriteLine("{0:f2}", farenhait);
+        }
+    }
+}
