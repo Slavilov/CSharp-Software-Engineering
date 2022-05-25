fc42c51a5596dca949f63a7b1760e6e525cdb848
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-25 23:47:55 +0300

    Add files via upload

---
+++ C# Basics/More Exercises/04. Vegetable Market/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegPrice = double.Parse(Console.ReadLine());
+            double fruPrice = double.Parse(Console.ReadLine());
+            int vegWeight = int.Parse(Console.ReadLine());
+            int fruitWeight = int.Parse(Console.ReadLine());
+
+            double vegPriceK = vegPrice * vegWeight;
+            double fruitPriceK = fruPrice * fruitWeight;
+
+            double sum = (vegPriceK + fruitPriceK) / 1.94;
+            Console.WriteLine("{0:f2}", sum);
+
+        }
+    }
+}
