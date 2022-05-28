a23da5a23c15f96827bdd56ceca7ceffa2554c7b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-28 18:48:04 +0300

    Add files via upload

---
+++ C# Basics/More Exercises/05. Challenge the Wedding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace While1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int f = int.Parse(Console.ReadLine());
+            int tables = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= f; j++)
+                {
+                    counter++;
+                    if (counter <= tables)
+                    {
+                        Console.Write($"({i} <-> {j}) ");
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
+}
