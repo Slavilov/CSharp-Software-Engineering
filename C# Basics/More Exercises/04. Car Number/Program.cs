06b89ccb76b271c29d5cbb60e71f4e29084010cc
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-28 18:47:50 +0300

    Add files via upload

---
+++ C# Basics/More Exercises/04. Car Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int n1 = a; n1 <= b; n1++)
+            {
+                for (int n2 = a; n2 <= b; n2++)
+                {
+                    for (int n3 = a; n3 <= b; n3++)
+                    {
+                        for (int n4 = a; n4 <= b; n4++)
+                        {
+                            if (n1 > n4 && (n2 + n3) % 2 == 0)
+                            {
+                                if (n1 % 2 == 0 && n4 % 2 != 0)
+                                {
+                                    Console.Write($"{n1}{n2}{n3}{n4} ");
+                                }
+                                else if (n1 % 2 != 0 && n4 % 2 == 0)
+                                {
+                                    Console.Write($"{n1}{n2}{n3}{n4} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
