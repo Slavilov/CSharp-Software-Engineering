3fcb98c3c5b8902145a256cfa0513cbe2ad5bd93
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-05-31 23:14:37 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int hrsInMins = hours * 60;
+            int sum = hrsInMins + minutes + 30;
+
+            int hrs = sum / 60;
+            int mins = sum % 60;
+
+            if (hrs >= 24)
+            {
+                hrs = 0;
+                Console.WriteLine($"{hrs}:{mins:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{hrs}:{mins:d2}");
+            }
+        }
+    }
+}
