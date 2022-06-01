8a81ddf83c2c65194b55f30e2a8edc0a7c0b2bb2
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-02 00:06:31 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/07. Theatre Promotion/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            switch (typeOfDay)
+            {
+                case "Weekday":
+                    if (age <= 18 && age >= 0)
+                    {
+                        Console.WriteLine("12$");
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        Console.WriteLine("18$");
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        Console.WriteLine("12$");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    break;
+                case "Weekend":
+                    if (age <= 18 && age >= 0)
+                    {
+                        Console.WriteLine("15$");
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        Console.WriteLine("20$");
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        Console.WriteLine("15$");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    break;
+                case "Holiday":
+                    if (age <= 18 && age >= 0)
+                    {
+                        Console.WriteLine("5$");
+                    }
+                    else if (age > 18 && age <= 64)
+                    {
+                        Console.WriteLine("12$");
+                    }
+                    else if (age > 64 && age <= 122)
+                    {
+                        Console.WriteLine("10$");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    break;
+            }
+        }
+    }
+}
