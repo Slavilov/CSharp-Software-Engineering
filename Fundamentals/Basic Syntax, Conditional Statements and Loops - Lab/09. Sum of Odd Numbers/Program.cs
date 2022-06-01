6f1bfbb7d34047c5e3a052491464dfbdb648ab97
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-02 00:07:07 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int counter = 0;
+
+            for (int i = 1; i <= int.MaxValue; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    counter++;
+                    Console.WriteLine(i);
+                    sum += i;
+                    if (counter == n)
+                    {
+                        Console.WriteLine($"Sum: {sum}");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
