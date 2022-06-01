201db21e194a02ee861ad0bf668339265288a49a
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-02 00:07:45 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            if (m > 10)
+            {
+                Console.WriteLine($"{n} X {m} = {n * m}");
+                return;
+            }
+            for (m = m; m <= 10; m++)
+            {
+                Console.WriteLine($"{n} X {m} = {n * m}");
+            }
+        }
+    }
+}
