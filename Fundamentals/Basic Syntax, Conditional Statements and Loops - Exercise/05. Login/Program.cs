6cdab22e3ee83e20a813027ef79029af52a3deeb
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-03 23:56:18 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/05. Login/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+            int counter = 0;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            string pass = Console.ReadLine();
+            while (pass != password)
+            {
+                counter++;
+                if (counter == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+                pass = Console.ReadLine();
+            }
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
