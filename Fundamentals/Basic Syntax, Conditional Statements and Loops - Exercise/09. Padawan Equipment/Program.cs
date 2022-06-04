e0ca9006af2aaa1ff0c126c7bdaa814d40d0713c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-04 23:07:08 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int student = int.Parse(Console.ReadLine());
+            double priceOfSaber = double.Parse(Console.ReadLine());
+            double priceOfRobe = double.Parse(Console.ReadLine());
+            double priceOfBelt = double.Parse(Console.ReadLine());
+
+            double sumOfAll = 0;
+            //double lightSabers10 = priceOfSaber * (student + Math.Ceiling(student * 0.10));
+            double beltDiscount = priceOfBelt * (student / 6);
+
+            sumOfAll = ((priceOfSaber * (student + Math.Ceiling(student * 0.10)) + (priceOfRobe * student) + ((priceOfBelt * student) - beltDiscount)));
+
+            if (sumOfAll <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {sumOfAll:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {(sumOfAll - money):f2}lv more.");
+            }
+        }
+    }
+}
