15f0bb37dead9d4ebc66b4c470b6a606c1fc5daf
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-04 23:06:12 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong number/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string nString = n.ToString();
+            double sum = 1;
+            double counter = 0;
+
+            for (int i = 0; i < nString.Length; i++)
+            {
+                sum = 1;
+                char a = nString[i];
+                int b = a - '0';
+
+                if (b == 0)
+                {
+                    counter = counter + 1;
+                    continue;
+                }
+
+                for (int j = 1; j <= b; j++)
+                {
+                    sum = sum * j;
+                }
+                counter += sum;
+            }
+            if (counter == n)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
