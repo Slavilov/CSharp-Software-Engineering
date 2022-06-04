60b9e3923145852b6db4c67fa14f257b7b4fcd6e
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-04 23:07:33 +0300

    Add files via upload

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/11. Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sumForDay = 0;
+            double sumForAll = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                double capsulePrice = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsuleCount = int.Parse(Console.ReadLine());
+                sumForDay = ((days * capsuleCount) * capsulePrice);
+                sumForAll += sumForDay;
+
+                Console.WriteLine($"The price for the coffee is: ${((days * capsuleCount) * capsulePrice):f2}");
+                sumForDay = 0;
+            }
+            Console.WriteLine($"Total: ${sumForAll:f2}");
+        }
+    }
+}
