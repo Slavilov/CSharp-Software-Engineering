268b4db63c8dcab70e9e6306797a35abcb5fa07e
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-06 11:06:32 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GBPtoUSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal gbp = decimal.Parse(Console.ReadLine());
+            decimal usd = gbp * 1.31m;
+            Console.WriteLine($"{usd:f3}");
+        }
+    }
+}
