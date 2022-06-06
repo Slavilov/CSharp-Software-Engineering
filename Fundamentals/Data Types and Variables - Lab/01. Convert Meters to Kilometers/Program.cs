9b9c73bc0d2a5cf30e25f7551145e3b11d32033e
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-06 11:06:19 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertMetersToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double km = meters * 0.001;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
