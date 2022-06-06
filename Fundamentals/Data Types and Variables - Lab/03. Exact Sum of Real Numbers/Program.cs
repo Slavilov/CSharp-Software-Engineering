02a29018c65f6a73988d8b588c21d1cca8af066f
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-06 11:07:55 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumOfRealNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                decimal d = decimal.Parse(Console.ReadLine());
+                sum += d;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
