9072b1b433ef736149b9a0cad27ff8c173f6e121
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-06 11:08:10 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Lab/05. Special Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SpecialNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool correct = false;
+            for (int num = 1; num <= n; num++)
+            {
+                int sumOfDigits = 0;
+                int digits = num;
+
+                while (digits > 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    correct = true;
+                    Console.WriteLine($"{num} -> {correct}");
+                }
+                else
+                {
+                    correct = false;
+                    Console.WriteLine($"{num} -> {correct}");
+                }
+            }
+        }
+    }
+}
