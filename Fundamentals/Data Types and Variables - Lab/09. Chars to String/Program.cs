946b23933afb2a1af57f877be4acb87586826530
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-07 18:06:13 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Lab/09. Chars to String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+            string sum = "";
+            sum = sum + first + second + third;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
