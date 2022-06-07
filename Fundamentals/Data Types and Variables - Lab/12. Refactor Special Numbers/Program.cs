c5eb8d51072a2c6135fe2b2e3bc0c0fe6203f621
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-07 18:06:58 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace RefactorSpecialNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int number = 0;
+
+            bool specialNum = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                number = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                specialNum = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine("{0} -> {1}", number, specialNum);
+                sum = 0;
+                i = number;
+            }
+        }
+    }
+}
