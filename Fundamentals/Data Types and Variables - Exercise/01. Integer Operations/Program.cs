0323d42cce3d7bc524ae853eb59a0273d5c5bc1f
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-09 23:45:27 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int sumOne = firstNum + secondNum;
+
+            int thirdNum = int.Parse(Console.ReadLine());
+            int sumTwo = sumOne / thirdNum;
+
+
+            int fourthNum = int.Parse(Console.ReadLine());
+            int sumThree = sumTwo * fourthNum;
+
+            Console.WriteLine(sumThree);
+        }
+    }
+}
