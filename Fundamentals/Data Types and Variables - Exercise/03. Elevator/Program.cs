d222f9c79a245e835a8e7d77ec3465d7d12a3fbe
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-09 23:46:16 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPpl = int.Parse(Console.ReadLine());
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)numOfPpl / maxCapacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
