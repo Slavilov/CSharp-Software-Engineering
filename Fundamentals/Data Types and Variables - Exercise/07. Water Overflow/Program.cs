b3e2d8f7936d002c900fe9620adb7973ee963b63
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-10 23:23:03 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int tank = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int inputLeters = int.Parse(Console.ReadLine());
+                if (inputLeters > 255 || (inputLeters + tank) > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    tank += inputLeters;
+                }
+            }
+            Console.WriteLine(tank);
+        }
+    }
+}
