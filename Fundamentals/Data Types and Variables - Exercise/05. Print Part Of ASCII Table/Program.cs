535340d6727862a8844bdd047113afd200a85404
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-10 23:22:19 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,19 @@
+﻿// See husing System;
+using System.Text;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int stop = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= stop; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
