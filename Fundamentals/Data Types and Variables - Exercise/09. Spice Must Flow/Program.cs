c977e64370b81cbb06fc8013d35b8776ce430b28
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-10 23:23:54 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int daysCounter = 0;
+            int totalAmountExtracted = 0;
+
+            while (startingYield >= 100)
+            {
+                daysCounter++;
+                totalAmountExtracted = (totalAmountExtracted + startingYield) - 26;
+                startingYield -= 10;
+            }
+            totalAmountExtracted = totalAmountExtracted - 26;
+            Console.WriteLine(daysCounter);
+            Console.WriteLine(totalAmountExtracted);
+        }
+    }
+}
