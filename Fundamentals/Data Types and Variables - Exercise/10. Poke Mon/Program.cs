1aab12232789b5c90b7a0f35610f2a6ecf4e8b70
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-10 23:24:38 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int pokePowerOriginal = N;
+
+            int M = int.Parse(Console.ReadLine());
+            byte Y = byte.Parse(Console.ReadLine());
+
+            int pokesCounter = 0;
+
+            while (N >= M)
+            {
+                pokesCounter++;
+                N = N - M;
+
+                if ((decimal)N == ((decimal)(pokePowerOriginal * 0.50)))
+                {
+                    if (N / Y >= 0 && Y > 0)
+                    {
+                        N = N / Y;
+                    }
+                    else
+                    {
+                        N = N - M;
+                    }
+                }
+            }
+            Console.WriteLine(N);
+            Console.WriteLine(pokesCounter);
+        }
+    }
+}
