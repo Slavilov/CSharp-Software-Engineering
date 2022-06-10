5e038dc697d000a83e4fcf4881882a7eb41ae107
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-10 23:23:27 +0300

    Add files via upload

---
+++ Fundamentals/Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nKegs = int.Parse(Console.ReadLine());
+            string biggestKeg = string.Empty;
+            double volume = 0;
+            double maxVolume = 0;
+
+            for (int j = 1; j <= nKegs; j++)
+            {
+                string kegName = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                volume = Math.PI * radius * radius * height;
+
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    biggestKeg = kegName;
+                }
+
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
