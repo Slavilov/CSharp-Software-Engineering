938ce7a63b694f3cfa9e29ff50677cfd32a179a5
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-11 23:41:21 +0300

    Add files via upload

---
+++ Fundamentals/Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringNumbersArray = Console.ReadLine().Split();
+            double[] doubleNumbersArray = new double[stringNumbersArray.Length];
+
+            for (int i = 0; i < stringNumbersArray.Length; i++)
+            {
+                doubleNumbersArray[i] = double.Parse(stringNumbersArray[i]);
+            }
+
+            for (int i = 0; i < doubleNumbersArray.Length; i++)
+            {
+                if (doubleNumbersArray[i] == 0)
+                {
+                    doubleNumbersArray[i] = 0;
+                }
+                Console.WriteLine($"{doubleNumbersArray[i]} => {(int)Math.Round(doubleNumbersArray[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
