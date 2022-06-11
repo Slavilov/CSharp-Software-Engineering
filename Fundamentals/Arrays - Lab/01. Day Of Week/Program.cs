f9db331b7282e0213ee715ce956b765692e355e6
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-11 23:40:08 +0300

    Add files via upload

---
+++ Fundamentals/Arrays - Lab/01. Day Of Week/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (n >= 1 && n <= 7)
+            {
+                Console.WriteLine(daysOfWeek[n - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+        }
+    }
+}
