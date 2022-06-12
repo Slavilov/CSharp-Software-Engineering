472cd7558ad8b3a4206cec1b4e5b5ccc1e05a83c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-12 22:34:13 +0300

    Add files via upload

---
+++ Fundamentals/Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                string temp = array[array.Length - 1 - i];
+
+                array[array.Length - 1 - i] = array[i];
+                array[i] = temp;
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
