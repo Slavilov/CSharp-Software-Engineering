fa8a0ae8a75f1dee61c79bed7d01db15ead0f6bd
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-12 22:34:34 +0300

    Add files via upload

---
+++ Fundamentals/Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] numbers = new int[input.Length];
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                numbers[i] = int.Parse(input[i]);
+
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
