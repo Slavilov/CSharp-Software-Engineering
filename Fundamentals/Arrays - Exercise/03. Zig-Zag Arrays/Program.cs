288eb2f0bf4d6c87ef7c44179974e26824b1c062
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-13 22:55:27 +0300

    Add files via upload

---
+++ Fundamentals/Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numberOne = 0;
+            int numberTwo = 0;
+            int[] arrayOne = new int[n];
+            int[] arrayTwo = new int[n];
+            int counterNumOne = 1;
+            for (int i = 0; i < n; i++)
+            {
+                counterNumOne++;
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                numberOne = input[0];
+                numberTwo = input[1];
+                if (counterNumOne % 2 == 0)
+                {
+                    arrayOne[i] = numberOne;
+                    arrayTwo[i] = numberTwo;
+                }
+                else
+                {
+                    arrayOne[i] = numberTwo;
+                    arrayTwo[i] = numberOne;
+                }
+
+            }
+            Console.WriteLine(String.Join(' ', arrayOne));
+            Console.WriteLine(String.Join(' ', arrayTwo));
+
+        }
+    }
+}
