4fe7f7d59392521ca5fa47fd7f9149532ef9e254
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-13 22:54:51 +0300

    Add files via upload

---
+++ Fundamentals/Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] wagons = new int[n];
+            int sumOfPassengers = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+                sumOfPassengers += wagons[i];
+            }
+
+            foreach (int number in wagons)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sumOfPassengers);
+
+        }
+    }
+}
