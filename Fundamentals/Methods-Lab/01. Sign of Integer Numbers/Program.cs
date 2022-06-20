c8d9d6b86ef319f1eaa25a122b77e1cdaeb2ba5e
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-20 23:34:51 +0300

    Add files via upload

---
+++ Fundamentals/Methods-Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNumbers(n);
+        }
+
+
+        static void PrintNumbers(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
