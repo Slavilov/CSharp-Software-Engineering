b84cef91d87e776044bdcef1d577e5163b638656
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-21 23:34:49 +0300

    Add files via upload

---
+++ Fundamentals/Methods-Lab/07. Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            string BigString = NewCombinedString(count, word);
+            Console.WriteLine(BigString);
+        }
+
+        static string NewCombinedString(int count, string word)
+        {
+            StringBuilder newString = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                newString.Append(word);
+            }
+            return newString.ToString();
+        }
+    }
+}
