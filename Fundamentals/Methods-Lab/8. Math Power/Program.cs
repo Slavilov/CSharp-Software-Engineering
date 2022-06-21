cff02d7993c4a4e1523b6bbb440c9c82cc872227
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-21 23:35:14 +0300

    Add files via upload

---
+++ Fundamentals/Methods-Lab/8. Math Power/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double sum = MathPower(number, power);
+            Console.WriteLine(sum);
+
+        }
+
+        static double MathPower(double number, int power)
+        {
+            double result = 1;
+            for (int i = 1; i <= power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+    }
+}
