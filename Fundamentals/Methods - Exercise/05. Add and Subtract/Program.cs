e829fd63ca461c642522c75129c46824c02bed59
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-23 23:03:27 +0300

    Add files via upload

---
+++ Fundamentals/Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace PasswordCheckUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            int sum = SumOfFirstTwo(numberOne, numberTwo);
+            Console.WriteLine(SubstractionFromTheSum(sum, numberThree));
+        }
+
+        static int SumOfFirstTwo(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        static int SubstractionFromTheSum(int sum, int numberThree)
+        {
+            return sum - numberThree;
+        }
+    }
+}
