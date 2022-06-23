fc575cf941e2605058c9d21312befcc5a767b053
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-23 23:20:48 +0300

    Add files via upload

---
+++ Fundamentals/Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace PasswordCheckUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(MiddleCharacters(input));
+        }
+
+        static string MiddleCharacters(string input)
+        {
+            string twoChars = string.Empty;
+            if (input.Length % 2 == 0)
+            {
+                for (int i = (input.Length / 2) - 1; i < (input.Length / 2) + 1; i++)
+                {
+                    twoChars += input[i];
+                }
+                return twoChars;
+
+            }
+            else
+            {
+                for (int i = (input.Length / 2); i < (input.Length / 2) + 1; i++)
+                {
+                    return input[i].ToString();
+                }
+            }
+            return twoChars;
+        }
+    }
+}
