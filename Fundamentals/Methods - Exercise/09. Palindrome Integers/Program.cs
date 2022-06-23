c73f6b52625ef6adb1d876f92a93dbf6f427e597
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-23 23:21:40 +0300

    Add files via upload

---
+++ Fundamentals/Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace PasswordCheckUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                PalindromeCheck(input);
+                input = Console.ReadLine();
+            }
+        }
+
+        static void PalindromeCheck(string input)
+        {
+            char[] stringArray = input.ToCharArray();
+            Array.Reverse(stringArray);
+            string reverseString = new string(stringArray);
+
+            if (input == reverseString)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
