ef0c4dae31396d5cff696f9aefdf275ff6424328
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-23 23:21:15 +0300

    Add files via upload

---
+++ Fundamentals/Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace PasswordCheckUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            MatrixPrinter(n);
+        }
+
+        static void MatrixPrinter(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
