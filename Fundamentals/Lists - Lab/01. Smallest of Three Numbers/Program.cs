b2d01e5e2ab7f4d9b75db95ba86d992da71f28a2
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-24 23:08:44 +0300

    Add files via upload

---
+++ Fundamentals/Lists - Lab/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SmallestOfThreeNums(numOne, numTwo, numThree));
+        }
+
+        static int SmallestOfThreeNums(int numOne, int numTwo, int numThree)
+        {
+            if (numOne < numTwo && numOne < numThree)
+            {
+                return numOne;
+            }
+            else if (numTwo < numOne && numTwo < numThree)
+            {
+                return numTwo;
+            }
+            else
+            {
+                return numThree;
+            }
+        }
+    }
+}
