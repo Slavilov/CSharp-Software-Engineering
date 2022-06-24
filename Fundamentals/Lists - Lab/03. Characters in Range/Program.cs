4247a15cc46d0b5b557ad52ecdb935d6e860382e
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-24 23:09:31 +0300

    Add files via upload

---
+++ Fundamentals/Lists - Lab/03. Characters in Range/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+            Console.WriteLine(CharactersInBetween(one, two));
+
+        }
+
+        static string CharactersInBetween(char one, char two)
+        {
+            //StringBuilder charsInBetween = new StringBuilder();
+            string newString = string.Empty;
+
+            if (two < one)
+            {
+                for (int i = two + 1; i < one; i++)
+                {
+                    newString += (char)i + " ";
+                }
+            }
+            else
+            {
+                for (int i = one + 1; i < two; i++)
+                {
+                    newString += (char)i + " ";
+                }
+            }
+            return newString;
+        }
+
+    }
+}
