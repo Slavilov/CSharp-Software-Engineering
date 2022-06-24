ef0450e4ee88e1f0e79192ea7d734ae79ef3779f
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-24 23:09:07 +0300

    Add files via upload

---
+++ Fundamentals/Lists - Lab/02. Vowels Count/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+
+namespace HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(NumberOfVowels(input));
+        }
+
+        static int NumberOfVowels(string word)
+        {
+            int result = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                switch (word[i])
+                {
+                    case 'a':
+                    case 'A':
+                    case 'e':
+                    case 'E':
+                    case 'i':
+                    case 'I':
+                    case 'o':
+                    case 'O':
+                    case 'u':
+                    case 'U':
+                        result++;
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
