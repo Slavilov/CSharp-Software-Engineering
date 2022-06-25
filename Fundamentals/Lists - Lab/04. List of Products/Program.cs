f9beab107d906e8f2ffdd09eca8912f3e6530188
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-25 23:00:05 +0300

    Add files via upload

---
+++ Fundamentals/Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PasswordCheckUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            list.Sort();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}.{list[i]}");
+            }
+        }
+    }
+}
