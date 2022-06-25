97c6aa0ffe17d8df5aa7306ccc8a2a18a50449c2
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-25 23:00:20 +0300

    Add files via upload

---
+++ Fundamentals/Lists - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PasswordCheckUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i = i - 1;
+                }
+            }
+            numbers.Reverse();
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
