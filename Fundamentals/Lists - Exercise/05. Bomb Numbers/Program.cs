d11b5dcdaac4a3ccf4ba9f656317a5ac13954e62
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-06-27 22:41:44 +0300

    Add files via upload

---
+++ Fundamentals/Lists - Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PasswordCheckUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> bombPower = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bombPower[0])
+                {
+                    if (i + bombPower[1] > numbers.Count - 1)
+                    {
+                        numbers.RemoveRange((i - bombPower[1]), (1 + ((2 * bombPower[1])) - (numbers.Count - i)));
+                        i = 0;
+                    }
+                    else if (i - bombPower[1] < 0)
+                    {
+                        numbers.RemoveRange(0, (1 + (2 * bombPower[1]) - (i - 1)));
+                        i = 0;
+                    }
+                    else
+                    {
+                        numbers.RemoveRange((i - bombPower[1]), (1 + (2 * bombPower[1])));
+                        i = 0;
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
