746edcfe8e5530b66a629a299ee421010a4751fc
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-04 23:30:28 +0300

    Add files via upload

---
+++ Fundamentals/Objects and Classes - Exercise/07. Order by Age/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SongsExerciseForObjectsAndClasses
+{
+    public class Person
+    {
+        public Person(string name, string id, int age)
+        {
+            this.NameOfPerson = name;
+            this.IdOfPerson = id;
+            this.AgeOfPerson = age;
+        }
+
+        public string NameOfPerson { get; set; }
+        public string IdOfPerson { get; set; }
+        public int AgeOfPerson { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputCommands = new List<string>();
+            List<Person> listOfPersons = new List<Person>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+
+                inputCommands = input.Split(' ').ToList();
+
+                string name = inputCommands[0];
+                string id = inputCommands[1];
+                int age = int.Parse(inputCommands[2]);
+
+                if (listOfPersons.Exists(x => x.IdOfPerson == id))
+                {
+                    int index = listOfPersons.FindIndex(x => x.IdOfPerson == id);
+
+                    listOfPersons[index].NameOfPerson = name;
+                    listOfPersons[index].AgeOfPerson = age;
+                    continue;
+                }
+
+                Person newPerson = new Person(name, id, age);
+                listOfPersons.Add(newPerson);
+            }
+            List<Person> sortedList = listOfPersons.OrderBy(x => x.AgeOfPerson).ToList();
+
+            foreach (Person person in sortedList)
+            {
+                Console.WriteLine($"{person.NameOfPerson} with ID: {person.IdOfPerson} is {person.AgeOfPerson} years old.");
+            }
+        }
+    }
+}
