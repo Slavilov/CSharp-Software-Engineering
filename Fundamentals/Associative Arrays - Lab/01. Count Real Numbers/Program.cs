421c236931ebaf989284681e04481c4206e5d4aa
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-05 23:37:18 +0300

    Add files via upload

---
+++ Fundamentals/Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AssociativeArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputOfIntegers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            SortedDictionary<int, int> result = new SortedDictionary<int, int>();
+
+            foreach (var number in inputOfIntegers)
+            {
+                if (result.ContainsKey(number))
+                {
+                    result[number]++;
+                }
+                else
+                {
+                    result.Add(number, 1);
+                }
+            }
+
+            foreach (var pair in result)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
