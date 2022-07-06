8ae4946f94c393dc64889c920cbfadde9a51a620
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-06 23:47:48 +0300

    Add files via upload

---
+++ Fundamentals/Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AssociativeArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+            string[] newWords = words.Where(n => n.Length % 2 == 0).ToArray();
+
+            foreach (var word in newWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
