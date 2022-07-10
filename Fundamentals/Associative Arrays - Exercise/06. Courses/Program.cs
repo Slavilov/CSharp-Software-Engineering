24e35a8fec72f3ca22cbcc6b5bf2c25b0ed3199c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-10 22:58:22 +0300

    Add files via upload

---
+++ Fundamentals/Associative Arrays - Exercise/06. Courses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AssociativeArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                List<string> inputList = input.Split(" : ").ToList();
+                string courseName = inputList[0];
+                string studentName = inputList[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>());
+                    courses[courseName].Add(studentName);
+                }
+                else
+                {
+                    courses[courseName].Add(studentName);
+                }
+            }
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+
+                foreach (var student in course.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
