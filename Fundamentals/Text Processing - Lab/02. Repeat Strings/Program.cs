e7c247e98e3b3dbeff425526475881a89a1b4abe
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-13 23:51:47 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AssociativeArraysExercise
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfStrings = Console.ReadLine().Split(' ').ToArray();
+            StringBuilder newString = new StringBuilder();
+
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+            {
+                int lenghtOfWord = arrayOfStrings[i].Count();
+
+                for (int j = 0; j < lenghtOfWord; j++)
+                {
+                    newString.Append(arrayOfStrings[i]);
+                }
+            }
+            Console.WriteLine(newString);
+        }
+    }
+}
