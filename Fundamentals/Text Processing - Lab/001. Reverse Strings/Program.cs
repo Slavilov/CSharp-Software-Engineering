6f35c46fea41b7073047ae22f06a066c2f47b3e1
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-13 23:51:23 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing - Lab/001. Reverse Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AssociativeArraysExercise
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                char[] newArray = input.ToCharArray();
+                Array.Reverse(newArray);
+                string newString = new string(newArray);
+                Console.WriteLine($"{input} = {newString}");
+            }
+        }
+    }
+}
