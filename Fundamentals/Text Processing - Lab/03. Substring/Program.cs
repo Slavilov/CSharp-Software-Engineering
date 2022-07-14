29cbc8d254a94f6fc010ba718a29a91c783c2efe
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-14 23:59:23 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AssociativeArraysExercise
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstStr = Console.ReadLine();
+            string secondStr = Console.ReadLine();
+
+            while (secondStr.Contains(firstStr))
+            {
+                int indexOfWord = secondStr.IndexOf(firstStr);
+                secondStr = secondStr.Remove(indexOfWord, firstStr.Length);
+            }
+            Console.WriteLine(secondStr);
+        }
+    }
+}
