5beb64d34a89cb0f19dc4ec077e63fb6e4c619e6
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-14 23:59:54 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AssociativeArraysExercise
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> bannedWords = Console.ReadLine().Split(", ").ToList();
+            string text = Console.ReadLine();
+            string replacement = string.Empty;
+            string newText = text;
+
+            for (int i = 0; i < bannedWords.Count; i++)
+            {
+                for (int j = 0; j < bannedWords[i].Length; j++)
+                {
+                    replacement += "*";
+                }
+                newText = newText.Replace(bannedWords[i], replacement);
+                replacement = "";
+            }
+            Console.WriteLine(newText);
+        }
+    }
+}
