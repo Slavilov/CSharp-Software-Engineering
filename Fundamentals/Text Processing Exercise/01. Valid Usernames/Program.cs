44073a41925671ce538536e61f3dda193e24feec
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-16 23:57:35 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AssociativeArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            bool flag = false;
+
+            foreach (var username in inputList)
+            {
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    foreach (char c in username)
+                    {
+                        if (char.IsLetterOrDigit(c) || c == '_' || c == '-')
+                        {
+                            flag = true;
+                        }
+                        else
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if (flag == true)
+                {
+                    Console.WriteLine(username);
+                }
+                flag = false;
+            }
+        }
+    }
+}
