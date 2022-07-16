18994606270fad1aca0e0c07a71f410992600387
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-16 23:14:25 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AssociativeArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string digits = "";
+            string letters = "";
+            string symbols = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (char.IsDigit(c))
+                {
+                    digits += c;
+                }
+                else if (char.IsLetter(c))
+                {
+                    letters += c;
+                }
+                else
+                {
+                    symbols += c;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(symbols);
+        }
+    }
+}
