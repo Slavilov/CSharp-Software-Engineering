7b3687bd419bb7ae7165513aadd480c368d71d1d
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-17 22:37:13 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing Exercise/03. Extract File/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AssociativeArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFileInput = Console.ReadLine();
+            char[] separator = { '\\', '.' };
+            List<string> listOfInput = pathFileInput.Split(separator, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Console.WriteLine($"File name: {listOfInput[listOfInput.Count - 2]}");
+            Console.WriteLine($"File extension: {listOfInput[listOfInput.Count - 1]}");
+        }
+    }
+}
