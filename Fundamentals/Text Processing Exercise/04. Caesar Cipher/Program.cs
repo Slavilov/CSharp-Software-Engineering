2102896ed2eefdf33d80227a91bc1820993ef9a8
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-17 22:38:06 +0300

    Add files via upload

---
+++ Fundamentals/Text Processing Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AssociativeArraysExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var ch in input)
+            {
+                int newCh = ((int)ch) + 3;
+                char secondCh = (char)newCh;
+                sb.Append(secondCh);
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
