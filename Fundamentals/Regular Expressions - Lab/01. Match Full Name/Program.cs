f438bf0d11cd088017514b2271692df3f0b5847e
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-18 23:58:21 +0300

    Add files via upload

---
+++ Fundamentals/Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegExExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]{1,} [A-Z][a-z]{1,}\b";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Value + " ");
+            }
+
+        }
+    }
+}
