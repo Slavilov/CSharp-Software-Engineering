4c326283048a3f99c6c7e8f762779d365eed5e79
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-20 22:36:50 +0300

    Add files via upload

---
+++ Fundamentals/Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegExExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(\d{2})(\.|-|\/)([A-Z][a-z]{2})\2(\d{4})\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match item in matches)
+            {
+                var date = item.Groups[1].Value;
+                var month = item.Groups[3].Value;
+                var year = item.Groups[4].Value;
+
+                Console.WriteLine($"Day: {date}, Month: {month}, Year: {year}");
+            }
+
+        }
+    }
+}
