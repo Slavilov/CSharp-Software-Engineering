75c875d47f028bf2f02dd0e159824abbe6550f7c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-22 23:31:45 +0300

    Add files via upload

---
+++ Fundamentals/Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegExExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> itemsBought = new List<string>();
+            double sum = 0;
+
+            while (true)
+            {
+                string inputText = Console.ReadLine();
+
+                if (inputText == "Purchase")
+                {
+                    break;
+                }
+
+                string pattern = @">>([A-Za-z]+)<<([0-9]+[.]?[0-9]+)[!](\d+)";
+
+                Regex regex = new Regex(pattern);
+                MatchCollection matches = Regex.Matches(inputText, pattern);
+
+
+                foreach (Match match in matches)
+                {
+                    itemsBought.Add(match.Groups[1].Value);
+                    sum += double.Parse(match.Groups[2].Value) * double.Parse(match.Groups[3].Value);
+                }
+            }
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in itemsBought)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
