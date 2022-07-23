20ef7ff1b9f3fae64d85810b39a9e61e6b348dc9
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-23 23:07:55 +0300

    Add files via upload

---
+++ Fundamentals/Regular Expressions - Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegExExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of shift")
+                {
+                    break;
+                }
+
+                string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+                Regex regex = new Regex(pattern);
+                MatchCollection matches = Regex.Matches(input, pattern);
+
+                foreach (Match item in matches)
+                {
+                    var customer = item.Groups["customer"].Value;
+                    var product = item.Groups["product"].Value;
+                    int count = int.Parse(item.Groups["count"].Value);
+                    var price = double.Parse(item.Groups["price"].Value);
+
+                    Console.WriteLine($"{customer}: {product} - {count * price:f2}");
+                    sum += count * price;
+                }
+            }
+            Console.WriteLine($"Total income: {sum:f2}");
+        }
+    }
+}
