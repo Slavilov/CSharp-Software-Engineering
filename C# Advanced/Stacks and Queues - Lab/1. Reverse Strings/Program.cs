bc35b39c31ef878cf6f14a1e9517673aef9997d1
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-24 23:41:58 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues - Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stackOfChars = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stackOfChars.Push(input[i]);
+            }
+
+            while (stackOfChars.Count > 0)
+            {
+                StringBuilder temporaryChar = new StringBuilder();
+                temporaryChar.Append(stackOfChars.Pop());
+                Console.Write(temporaryChar);
+            }
+        }
+    }
+}
