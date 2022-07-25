c5f3ba3c4b64e6890b350fbfc33d92874c124197
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-25 23:16:15 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues - Lab/04. Matching Brackets/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SimpelCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> indexes = new Stack<int>();
+            int openingIndex = 0;
+            int closingIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    openingIndex = indexes.Pop();
+                    closingIndex = i + 1;
+                    Console.WriteLine(input.Substring(openingIndex, closingIndex - openingIndex));
+                }
+            }
+
+        }
+    }
+}
