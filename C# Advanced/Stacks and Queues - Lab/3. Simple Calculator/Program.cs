b4a84887724f0c4bfe2a60bfb2c3c648a7480c77
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-25 23:12:36 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SimpelCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsThatCanPass = int.Parse(Console.ReadLine());
+            Queue<string> queOfCars = new Queue<string>();
+            int counter = 0;
+
+            while(true)
+            {
+                string command = Console.ReadLine();
+                
+                if (command == "end")
+                {
+                    Console.WriteLine($"{counter} cars passed the crossroads.");
+                    break;
+                }
+                else if (command == "green")
+                {
+                    for (int i = 0; i<carsThatCanPass;i++)
+                    {
+                        if (queOfCars.Count == 0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine($"{queOfCars.Dequeue()} passed!");
+                        counter++;
+                    }
+                }
+                else
+                {
+                    queOfCars.Enqueue(command);
+                }
+            }
+        }
+    }
+}
