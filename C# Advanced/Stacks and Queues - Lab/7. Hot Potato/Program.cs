5df40dbf5e8e088f7c4ac19eef32fdbb245573b7
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-07-26 23:53:06 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kids = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string[] arrayOfKids = kids.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Queue<string> queOfKids = new Queue<string>();
+            int counter = 0;
+
+            for (int i = 0; i < arrayOfKids.Length; i++)
+            {
+                queOfKids.Enqueue(arrayOfKids[i]);
+            }
+
+
+            while (queOfKids.Count > 1)
+            {
+                counter++;
+                string temporaryKids = queOfKids.Dequeue();
+
+                if (counter % n == 0)
+                {
+                    Console.WriteLine($"Removed {temporaryKids}");
+                    continue;
+                }
+
+                queOfKids.Enqueue(temporaryKids);
+            }
+            Console.WriteLine($"Last is {queOfKids.Last()}");
+        }
+    }
+}
