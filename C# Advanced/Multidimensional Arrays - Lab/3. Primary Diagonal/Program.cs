4c8cfc793b750dfcaecbf8848226e47e85dbebf4
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-01 22:45:48 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfSquare = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[sizeOfSquare, sizeOfSquare];
+            int sumOfDiagonal = 0;
+            int counter = 0;
+
+            for (int row = 0; row < sizeOfSquare; row++)
+            {
+                string elements = Console.ReadLine();
+                int[] elementsArray = elements.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int column = 0; column < sizeOfSquare; column++)
+                {
+                    matrix[row, column] = elementsArray[column];
+
+
+                }
+
+            }
+
+            for (int row = 0; row < sizeOfSquare; row++)
+            {
+                for (int column = 0;column < sizeOfSquare; column++)
+                {
+                    if (counter == 0)
+                    {
+                        counter++;
+                        sumOfDiagonal += matrix[row, (column + row)];
+                    }
+                }
+                counter = 0;
+            }
+            Console.WriteLine(sumOfDiagonal);
+        }
+    }
+}
