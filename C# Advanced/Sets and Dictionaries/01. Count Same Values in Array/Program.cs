9c15ca4bf4c44d6b3273c2d20374c9075633bd4b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-07 23:34:59 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<double> listOfNumbers = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+            Dictionary<double, int> numbersOccurences = new Dictionary<double, int>();
+            int counter = 0;
+
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+
+                if (numbersOccurences.ContainsKey(listOfNumbers[i]))
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < listOfNumbers.Count; j++)
+                {
+                    if (listOfNumbers[i] == listOfNumbers[j])
+                    {
+                        if (numbersOccurences.ContainsKey(listOfNumbers[i]))
+                        {
+                            numbersOccurences[listOfNumbers[i]]++;
+                        }
+                        else
+                        {
+                            numbersOccurences.Add(listOfNumbers[i], 1);
+                        }
+                    }
+                }
+            }
+
+            foreach(var number in numbersOccurences)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times ");
+            }
+        }
+    }
+}
