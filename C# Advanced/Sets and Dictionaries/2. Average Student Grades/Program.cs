0094550b716a1b8bbc5bdad4f67d0cb7cc6555f4
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-09 22:59:08 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries/2. Average Student Grades/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AverageGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studentGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i< n; i++)
+            {
+                List<string> inputList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                string student = inputList[0];
+                decimal grade = decimal.Parse(inputList[1]);
+
+                if (studentGrades.ContainsKey(student))
+                {
+                    studentGrades[student].Add(grade);
+                }
+                else
+                {
+                    studentGrades.Add(student, new List<decimal>());
+                    studentGrades[student].Add(grade);
+                }
+            }
+
+            foreach(var student in studentGrades)
+            {
+                Console.Write($"{student.Key} -> {string.Join(" ", student.Value.Select(v=>v.ToString("F2")))} (avg: {student.Value.Average():f2})");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
