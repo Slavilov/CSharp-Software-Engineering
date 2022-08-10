65da33906ba3814edc0997747c0b7d3b039ff090
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-10 23:20:00 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries/6. Record Unique Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace UniqeNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
