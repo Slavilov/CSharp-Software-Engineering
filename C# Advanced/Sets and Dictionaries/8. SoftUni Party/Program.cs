b781ddce81f1f40f429929b38c9fcdd2d9492156
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-11 23:42:52 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries/8. SoftUni Party/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace UniqeNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> guests = new SortedSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "PARTY")
+                {
+                    while (true)
+                    {
+                        string secondInput = Console.ReadLine();
+                        if (secondInput == "END")
+                        {
+                            Console.WriteLine(guests.Count);
+
+                            foreach (string s in guests)
+                            {
+                                Console.WriteLine(s);
+                            }
+                            return;
+                        }
+                        guests.Remove(secondInput);
+                    }
+                }
+
+                guests.Add(input);
+            }
+        }
+    }
+}
