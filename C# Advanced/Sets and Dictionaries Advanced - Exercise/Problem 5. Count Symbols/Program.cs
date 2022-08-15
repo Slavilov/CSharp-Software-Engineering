4844bb2088b4656a6cad39c8de906354fce89301
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-15 22:49:26 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/Problem 5. Count Symbols/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> symbolCounter = new SortedDictionary<char, int>();
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (symbolCounter.ContainsKey(text[i]))
+                {
+                    symbolCounter[text[i]]++;
+                }
+                else
+                {
+                    symbolCounter.Add(text[i], 1);
+                }
+            }
+
+            foreach (var symbol in symbolCounter)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
+
