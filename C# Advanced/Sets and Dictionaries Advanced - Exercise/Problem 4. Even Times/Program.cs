377f5374c44019a808191f38ffdb16c7710cd6af
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-15 22:49:42 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/Problem 4. Even Times/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (numbers.ContainsKey(number))
+                {
+                    numbers[number]++;
+                }
+                else
+                {
+                    numbers.Add(number, 1);
+                }
+            }
+
+            foreach (var number in numbers) 
+            {
+                if (number.Value % 2 == 0)
+                {
+                    Console.WriteLine(number.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
