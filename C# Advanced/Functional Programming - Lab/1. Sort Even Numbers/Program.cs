a078315eba3f9f4b84d93ed0c2610872de16d8df
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-17 23:03:31 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Lab/1. Sort Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine().Split(", ").Select(double.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
