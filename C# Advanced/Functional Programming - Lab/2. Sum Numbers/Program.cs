ab43f8139587bddc70643cbd13c59dc13edb4c15
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-17 23:03:53 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Lab/2. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SumNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            Console.WriteLine(nums.Count);
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
