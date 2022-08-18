b63eb60e56294181389356c946a740e9a788df58
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-18 23:17:57 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Lab/4. Add VAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AddVat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> addVat = a => a + (a * 0.20);
+            List<double> prices = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).Select(addVat).ToList();
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
