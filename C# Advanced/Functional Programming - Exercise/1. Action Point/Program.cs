661eaa40154d560e4f253b52b8eb59f25d4b7eda
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-21 23:48:09 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Exercise/1. Action Point/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> action = (x) => Console.WriteLine(x);
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (var name in names)
+            {
+                action(name);
+            }
+
+        }
+    }
+}
