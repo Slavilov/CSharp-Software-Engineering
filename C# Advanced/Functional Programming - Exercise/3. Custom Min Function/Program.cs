54c1444d17343429638453e963d12728d1fdd945
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-23 23:35:44 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Exercise/3. Custom Min Function/Program.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Func<int[], int> minimumNumDelegate = minNum;
+            Console.WriteLine(minimumNumDelegate(nums));
+        }
+
+        static int minNum(int[]nums)
+        {
+            int min = int.MaxValue;
+
+            foreach (var num in nums)
+            {
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            return min;
+        }
+    }
+}
+
