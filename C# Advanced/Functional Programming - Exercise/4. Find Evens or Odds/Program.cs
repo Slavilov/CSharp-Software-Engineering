ea742af8d14a18f4e667f121c147aa2c42f75fc6
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-24 23:12:55 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Exercise/4. Find Evens or Odds/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string operation = Console.ReadLine();
+
+            Predicate<int> isEven = n => n % 2 == 0;
+            Predicate<int> isOdd = n => n % 2 != 0;
+
+            List<int> numsTwo = new List<int>();
+
+            for (int i = nums[0]; i <= nums[1]; i++)
+            {
+                numsTwo.Add(i);
+            }
+            List<int> numsThree = new List<int>();
+
+
+            if (operation == "odd")
+            {
+               numsThree = numsTwo.FindAll(isOdd);
+            }
+            else
+            {
+                numsThree = numsTwo.FindAll(isEven);
+            }
+
+            Console.WriteLine(string.Join(' ', numsThree));
+        }
+    }
+}
