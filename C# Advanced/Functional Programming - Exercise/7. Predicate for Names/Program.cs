45578df8f1700f0ee5fddc6e50dae5432f9c4c72
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-25 23:44:01 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Exercise/7. Predicate for Names/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= n)
+                .ToList();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
+
+
+
+ 
