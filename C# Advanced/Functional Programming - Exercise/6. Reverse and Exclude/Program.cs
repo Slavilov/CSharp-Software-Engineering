601e71d418ecac0369db0440a969cf0a707c0665
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-25 23:43:36 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Exercise/6. Reverse and Exclude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+            numbers.Reverse();
+            List<int> newNumbers = numbers.FindAll(x => (x % n) != 0);
+            Console.Write(string.Join(' ',newNumbers));
+        }
+    }
+}
+
+
