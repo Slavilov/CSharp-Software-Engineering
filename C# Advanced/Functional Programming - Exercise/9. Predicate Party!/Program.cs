dee69c6897baee5c0f8edc70a44a70550a27abb9
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-27 23:13:07 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Exercise/9. Predicate Party!/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
