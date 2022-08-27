60c2bc00fdac50d9b4e56f5f0ff7c4054d55ad6b
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-27 23:12:45 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming - Exercise/8. List of Predicates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SortEvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                numbers.Add(i);
+            }
+
+            List<int> dividers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            bool flag = false;
+
+            foreach (var number in numbers)
+            {
+                foreach (var divider in dividers)
+                {
+                    if (number % divider == 0)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
+
+
+
+
