b7cb2d11bdad5f840ec8f75219886a9a7b9a899c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-29 23:08:00 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes - Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
