84ac7863182ca0e2ea29e910971fb43bc9850315
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-29 23:07:37 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes - Lab/01. Car/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CarManufacturer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "BMW";
+            car.Model = "M3";
+            car.Year = 2016;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
