83b2eb8dbc635419ee306cd3f00a3f07abb2d3c4
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-08-31 23:13:41 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes - Lab/3. Car Constructors/Car.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) :this()
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double remainingFuel = (this.FuelQuantity - distance) * fuelConsumption;
+            if (remainingFuel >= 0)
+            {
+                this.FuelQuantity = remainingFuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string x = $"Make: {this.Make}" +
+                $"Model: {this.Model}" +
+                $"Year: {this.Year}" +
+                $"Fuel: {this.FuelQuantity:f2}";
+            return x;
+        }
+
+    }
+}
+
---
+++ C# Advanced/Defining Classes - Lab/3. Car Constructors/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+            Console.WriteLine($"FirstCar -> Make: {firstCar.Make}" +
+                $"Model: {firstCar.Model}" +
+                $"Year: {firstCar.Year}" +
+                $"FuelQuantity: {firstCar.FuelQuantity}" +
+                $"FuelConsumption: {firstCar.FuelConsumption}");
+                
+            Console.WriteLine($"SecondCar -> Make: {secondCar.Make}" +
+                $"Model: {secondCar.Model}" +
+                $"Year: {secondCar.Year}" +
+                $"FuelQuantity: {secondCar.FuelQuantity}" +
+                $"FuelConsumption: {secondCar.FuelConsumption}");
+
+            Console.WriteLine($"ThirdCar -> Make: {thirdCar.Make}" +
+                $"Model: {thirdCar.Model}" +
+                $"Year: {thirdCar.Year}" +
+                $"FuelQuantity: {thirdCar.FuelQuantity}" +
+                $"FuelConsumption: {thirdCar.FuelConsumption}");
+        }
+    }
+}
