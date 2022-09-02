fde365fe8acbda551f56e48afbd421f6acfa66a9
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-02 23:44:57 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes - Lab/5. Special Cars/Car.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+
+
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+
+
+
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+
+        public double TiresPressure(Tire[] tires)
+        {
+            double sumOfPressure = 0;
+            foreach (var tire in tires)
+            {
+                sumOfPressure += tire.Pressure;
+            }
+            return sumOfPressure;
+        }
+
+        public void Drive(double distance)
+        {
+            double remainingFuel = (this.FuelQuantity - distance) * fuelConsumption;
+            if (remainingFuel >= 0)
+            {
+                this.FuelQuantity = remainingFuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string x = $"Make: {this.Make}" +
+                $"Model: {this.Model}" +
+                $"Year: {this.Year}" +
+                $"Fuel: {this.FuelQuantity:f2}";
+            return x;
+        }
+
+    }
+}
+
+
+
