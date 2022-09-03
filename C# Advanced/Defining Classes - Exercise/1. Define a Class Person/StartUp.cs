2dbe46008612fa3c83a5b0e2d0f55eadfc43ed37
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-03 23:49:09 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes - Exercise/1. Define a Class Person/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            firstPerson.Age = 20;
+            firstPerson.Name = "Peter";
+
+            Console.WriteLine($"First Person Name: {firstPerson.Name}");
+            Console.WriteLine($"First Person Age: {firstPerson.Age}");
+
+            Person secondPerson = new Person(Console.ReadLine(), int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Second Person Name: {secondPerson.Name}");
+            Console.WriteLine($"Second Person Age: {secondPerson.Age}");
+        }
+    }
+}
