c294f8b9fad2372c2a731a9ce3194618a837d905
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-06 23:14:57 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes - Exercise/5. Date Modifier/DateModifier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public DateModifier(string start, string end)
+        {
+            this.startDate = start;
+            this.endDate = end;
+        }
+
+        private string startDate;
+        private string endDate;
+
+        public string StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        public string EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public int DifferenceBetweenTwoDates(string startDate, string endDate)
+        {
+            int difference = 0;
+            string newStartDate = startDate.Replace(' ', '/');
+            string newEndDate = endDate.Replace(' ', '/');
+            DateTime start = DateTime.Parse(newStartDate);
+            DateTime end = DateTime.Parse(newEndDate);
+
+            return difference = (int)(end - start).TotalDays;
+        }
+
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/5. Date Modifier/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier(firstDate, secondDate);
+
+            Console.WriteLine(Math.Abs(dateModifier.DifferenceBetweenTwoDates(firstDate, secondDate)));
+
+            LinkedList<int> linkiranlist = new LinkedList<int>();
+        }
+    }
+}
