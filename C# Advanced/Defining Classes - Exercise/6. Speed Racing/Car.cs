b7ba16a232a3638aab4e218b51c69b646a636f90
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-06 23:15:28 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes - Exercise/6. Speed Racing/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumptionPerKilometer = fuelConsumptionPerKm;
+        }
+
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance = 0;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public void Drive(int km)
+        {
+            if (this.fuelAmount / this.fuelConsumptionPerKilometer >= km)
+            {
+                this.fuelAmount = fuelAmount - (km * fuelConsumptionPerKilometer);
+                this.travelledDistance += km;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes - Exercise/6. Speed Racing/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> infoAboutCar = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                
+                string model = infoAboutCar[0];
+                double fuelAmount = double.Parse(infoAboutCar[1]);
+                double fuelConsumptionPerKm = double.Parse(infoAboutCar[2]);
+
+                Car newCar = new Car(model, fuelAmount, fuelConsumptionPerKm);
+                cars.Add(newCar);
+            }
+
+            while (true)
+            {
+                List<string> listOfCommands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                
+                if (listOfCommands[0] == "End")
+                {
+                    break;
+                }
+
+                if (listOfCommands[0] == "Drive")
+                {
+                    foreach (var car in cars)
+                    {
+                        if(car.Model == listOfCommands[1])
+                        {
+                            car.Drive(int.Parse(listOfCommands[2]));
+                        }
+                    }
+                }
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
