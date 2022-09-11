412a4943c0dd8e9df9d30edff5c469d9c26c1901
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-11 22:55:01 +0300

    Add files via upload

---
+++ C# Advanced/Generic - lab/2. Generic Array Creator/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfString = ArrayCreator.Create(5, "Velichko");
+        }
+    }
+}
