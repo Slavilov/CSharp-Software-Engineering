35310c62fb79b3dbe03f4c22af8687cfd245e655
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-11 22:54:44 +0300

    Add files via upload

---
+++ C# Advanced/Generic - lab/1. Box of T/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private readonly List<T> elements = new List<T>();
+
+        private int count = 0;
+
+        public int Counter
+        {
+            get { return count; }
+            private set { count = value; }
+        }
+
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+            count++;
+        }
+
+        public T Remove()
+        {
+            T result = elements[count-1];
+            count--;
+            elements.Remove(result);
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/Generic - lab/1. Box of T/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> tupak = new Box<int>();
+            tupak.Add(69);
+            tupak.Add(6969);
+            tupak.Add(666);
+            
+            Console.WriteLine(tupak.Remove());
+
+            tupak.Add(33);
+            tupak.Add(363363);
+
+            Console.WriteLine(tupak.Remove());
+        }
+    }
+}
