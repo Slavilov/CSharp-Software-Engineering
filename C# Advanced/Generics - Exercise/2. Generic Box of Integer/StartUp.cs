de9ef1b12b02686f73bac7fceeabfd34ea7c3a20
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-14 22:36:29 +0300

    Add files via upload

---
+++ C# Advanced/Generics - Exercise/2. Generic Box of Integer/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace GenericsExercise
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> newBox = new Box<int>();
+                newBox.AddToBox(input);
+                Console.WriteLine(newBox.ToString());
+            }
+        }
+    }
+}
