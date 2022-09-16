5f069c3cabb46c3621a623a41dd3e9ee47612a3c
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-16 22:40:40 +0300

    Add files via upload

---
+++ C# Advanced/Generics - Exercise/3. Generic Swap Method Strings/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BoxSwapper
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomList<Box<string>> customList = new CustomList<Box<string>>();
+            int numberOfBoxes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfBoxes; i++)
+            {
+                Box<string> newBox = new Box<string>();
+                newBox.AddToBox(Console.ReadLine());
+                customList.AddToCustomList(newBox);
+            }
+            List<int> inputNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int numOne = inputNumbers[0];
+            int numTwo = inputNumbers[1];
+
+            customList.BoxSwapper(numOne, numTwo);
+            customList.Foreach();
+        }
+    }
+}
