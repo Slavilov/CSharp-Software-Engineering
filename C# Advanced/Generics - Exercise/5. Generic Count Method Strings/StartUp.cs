dbdf1cc9974f7336e7dfad421bd406838af37f46
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-17 23:22:32 +0300

    Add files via upload

---
+++ C# Advanced/Generics - Exercise/5. Generic Count Method Strings/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BoxSwapper
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<string> newBox = new Box<string>();
+
+            int nOfElements = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nOfElements; i++)
+            {
+                newBox.AddToBox(Console.ReadLine());
+            }
+
+            string elementForComparison = Console.ReadLine();
+            Console.WriteLine(newBox.NumOfElementsLargerThanGivenElement(elementForComparison));
+        }
+    }
+}
