4a6289211ecc40e3c110b652e3d9bca6c7476bdf
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-17 23:23:00 +0300

    Add files via upload

---
+++ C# Advanced/Generics - Exercise/6. Generic Count Method Doubles/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxSwapper
+{
+    internal class Box<T> where T : IComparable
+    {
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        private List<T> elements;
+
+        public List<T> Elements
+        {
+            get { return elements; }
+            private set { elements = value; }
+        }
+
+        public void AddToBox(T element)
+        {
+            elements.Add(element);
+        }
+        public override string ToString()
+        {
+            return $"{elements[0].GetType()}: {elements[0]}";
+        }
+
+        public int NumOfElementsLargerThanGivenElement(T element)
+        {
+            int counter = 0;
+
+            foreach (var item in elements)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+}
---
+++ C# Advanced/Generics - Exercise/6. Generic Count Method Doubles/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BoxSwapper
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<double> newBox = new Box<double>();
+
+            int nOfElements = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nOfElements; i++)
+            {
+                newBox.AddToBox(double.Parse(Console.ReadLine()));
+            }
+
+            double elementForComparison = double.Parse(Console.ReadLine());
+            Console.WriteLine(newBox.NumOfElementsLargerThanGivenElement(elementForComparison));
+        }
+    }
+}
