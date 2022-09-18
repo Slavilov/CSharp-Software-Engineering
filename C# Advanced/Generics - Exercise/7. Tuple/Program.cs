518f4c1c26e3961cc4b5ce0e54356e820ac99864
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-18 23:36:24 +0300

    Add files via upload

---
+++ C# Advanced/Generics - Exercise/7. Tuple/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TupleExercise
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> firstInput = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+            string name = $"{firstInput[0]} {firstInput[1]}";
+            CustomTuple<string, string> stringTuple = new CustomTuple<string, string>(name, firstInput[2]);
+
+
+            List<string> secondInput = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+            CustomTuple<string, int> stringAndintTuple = new CustomTuple<string, int>(secondInput[0], int.Parse(secondInput[1]));
+
+
+            List<string> thirdInput = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+            CustomTuple<int, double> intAndDoubleTuple = new CustomTuple<int, double> (int.Parse(thirdInput[0]), double.Parse(thirdInput[1]));
+
+
+
+            Console.WriteLine($"{stringTuple.FirstElement} -> {stringTuple.SecondElement}");
+            Console.WriteLine($"{stringAndintTuple.FirstElement} -> {stringAndintTuple.SecondElement}");
+            Console.WriteLine($"{intAndDoubleTuple.FirstElement} -> {intAndDoubleTuple.SecondElement}");
+        }
+    }
+}
---
+++ C# Advanced/Generics - Exercise/7. Tuple/Tuple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleExercise
+{
+    public class CustomTuple<T1,T2>
+    {
+        public CustomTuple(T1 firstElement, T2 secondElement)
+        {
+            this.firstElement = firstElement;
+            this.secondElement = secondElement;
+        }
+
+        private T1 firstElement;
+        private T2 secondElement;
+
+        public T1 FirstElement
+        {
+            get { return firstElement; }
+            private set { firstElement = value; }
+        }
+
+        public T2 SecondElement
+        {
+            get { return secondElement; }
+           private set { secondElement = value; }
+        }
+
+    }
+}
