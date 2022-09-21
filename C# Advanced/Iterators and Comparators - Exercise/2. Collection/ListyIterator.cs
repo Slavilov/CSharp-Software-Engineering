9ab6f244dc568e5b85224e03b5a2065aaf44f046
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-21 23:02:54 +0300

    Add files via upload

---
+++ C# Advanced/Iterators and Comparators - Exercise/2. Collection/ListyIterator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterators_and_Comparators_Exercise
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        public ListyIterator(List<T> elements)
+        {
+            this.elements = elements;
+        }
+
+        private List<T> elements;
+        private int index = 0;
+
+        public List<T> Elements
+        {
+            get { return elements; }
+            set { elements = value; }
+        }
+
+        public int Index
+        {
+            get { return index; }
+            private set { index = value; }
+        }
+
+        public void Create(params T[] elements)
+        {
+            this.elements = new List<T>(elements);
+        }
+
+        public bool Move()
+        {
+            if (this.index < this.elements.Count - 1)
+            {
+                index++;
+                Console.WriteLine("True");
+                return true;
+            }
+            Console.WriteLine("False");
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.index < this.elements.Count - 1)
+            {
+                Console.WriteLine("True");
+                return true;
+            }
+            Console.WriteLine("False");
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.elements.Count > 0 && this.index > -1)
+            {
+                Console.WriteLine(this.elements[this.index]);
+            }
+            else
+            {
+                throw new Exception("Invalid Operation!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.elements.Count; i++)
+                yield return this.elements[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)elements).GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/2. Collection/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Iterators_and_Comparators_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> elements = Console.ReadLine().Split().Select(int.Parse).Skip(1).ToList();
+
+            ListyIterator<int> customCollection = new ListyIterator<int>(elements);
+
+            try
+            {
+                while (true)
+                {
+                    string command = Console.ReadLine();
+                    if (command == "Move")
+                    {
+                        customCollection.Move();
+                    }
+                    else if (command == "HasNext")
+                    {
+                        customCollection.HasNext();
+                    }
+                    else if (command == "Print")
+                    {
+                        customCollection.Print();
+                    }
+                    else if (command == "PrintAll")
+                    {
+                        foreach (var item in customCollection)
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
