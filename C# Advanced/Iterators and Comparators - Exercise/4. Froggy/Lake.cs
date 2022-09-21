bbe909fd95e3f40ec78a55efa2bcd4533d4b2873
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-21 23:03:18 +0300

    Add files via upload

---
+++ C# Advanced/Iterators and Comparators - Exercise/4. Froggy/Lake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._Froggy
+{
+    public class Lake<T> : IEnumerable<int>
+    {
+        public Lake(List<int> elements)
+        {
+            this.stones = elements;
+        }
+
+        private List<int> stones;
+
+        public List<int> Stones
+        {
+            get { return stones; }
+            set { stones = value; }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return stones[i];
+                }
+            }
+            for (int i = stones.Count - 1; i >= 0; i--)
+            {
+                if (i %2 != 0)
+                {
+                    yield return stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)stones).GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/4. Froggy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _4._Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Lake<int> customLake = new Lake<int>(numbers);
+
+            Console.WriteLine(string.Join(", ", customLake));
+        }
+    }
+}
