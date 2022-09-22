55cdcb0ec228850726c2658a2cad3b46990928c0
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-22 22:59:17 +0300

    Add files via upload

---
+++ C# Advanced/Iterators and Comparators - Exercise/6. Equality Logic/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._Equality_Logic
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            return this.Age.CompareTo(other.Age);
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Person person &&
+                   Name == person.Name &&
+                   Age == person.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Age);
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/6. Equality Logic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _6._Equality_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> setOfPpl = new SortedSet<Person>();
+            HashSet<Person> hashOfPpl = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                Person newPerson = new Person();
+                newPerson.Name = input[0].ToLower();
+                newPerson.Age = int.Parse(input[1]);
+
+                setOfPpl.Add(newPerson);
+                hashOfPpl.Add(newPerson);
+            }
+            Console.WriteLine(setOfPpl.Count);
+            Console.WriteLine(hashOfPpl.Count);
+        }
+    }
+}
