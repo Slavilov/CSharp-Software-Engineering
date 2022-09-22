335d25afcd2d299806aa69efc70806cd79fe2255
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-22 22:58:38 +0300

    Add files via upload

---
+++ C# Advanced/Iterators and Comparators - Exercise/5. Comparing Objects/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._Comparing_Objects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name == other.Name)
+            {
+                if (this.Age == other.Age)
+                {
+                    if (this.Town == other.Town)
+                    {
+                        return 0;
+                    }
+                    return -1;
+                }
+                return -1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/5. Comparing Objects/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _5._Comparing_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listOfPpl = new List<Person>();
+
+            while (true)
+            {
+                List<string> input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                if (input[0] == "END")
+                {
+                    break;
+                }
+
+                Person newPerson = new Person();
+                newPerson.Name = input[0];
+                newPerson.Age = int.Parse(input[1]);
+                newPerson.Town = input[2];
+
+                listOfPpl.Add(newPerson);
+            }
+            int positionOfPersonToCompare = int.Parse(Console.ReadLine());
+            Person comparer = listOfPpl[positionOfPersonToCompare-1];
+
+           //Person comparer = new Person();
+           //comparer.Name = listOfPpl[positionOfPersonToCompare - 1].Name;
+           //comparer.Age = listOfPpl[positionOfPersonToCompare - 1].Age;
+           //comparer.Town = listOfPpl[positionOfPersonToCompare - 1].Town;
+
+            int equal = 0;
+
+            foreach (var person in listOfPpl)
+            {
+                if (comparer.Name == person.Name)
+                {
+                    if (comparer.Age == person.Age)
+                    {
+                        if (comparer.Town == person.Town)
+                        {
+                            equal++;
+                        }
+                    }
+                }
+            }
+
+            if (equal > 1)
+            {
+                Console.WriteLine($"{equal} {listOfPpl.Count - equal} {listOfPpl.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
