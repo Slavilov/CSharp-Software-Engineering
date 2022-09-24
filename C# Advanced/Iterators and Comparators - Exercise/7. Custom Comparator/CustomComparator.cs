11824a1f48cb4e0bcce210edd82ba87641fa3996
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-24 23:19:33 +0300

    Add files via upload

---
+++ C# Advanced/Iterators and Comparators - Exercise/7. Custom Comparator/CustomComparator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._Custom_Comparator
+{
+    public class CustomComparator : IEnumerable
+    {
+        public CustomComparator(List<int> outsideElements)
+        {
+            this.elements = outsideElements;
+        }
+
+        private List<int> elements;
+
+        public List<int> Elements
+        {
+            get { return elements; }
+            set { elements = value; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return elements[i];
+                }
+            }
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return elements[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)elements).GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Exercise/7. Custom Comparator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _7._Custom_Comparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOtChisla = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            CustomComparator chisla = new CustomComparator(listOtChisla);
+
+            foreach (var item in chisla)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
