789f1c689fb8f84f96ea571af335a56a704ce39a
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-27 23:49:05 +0300

    Add files via upload

---
+++ C# OOP/Inheritance - Lab/3. Hierarchical Inheritance/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Farm
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy sharko = new Puppy();
+            sharko.Eat();
+            sharko.Bark();
+            sharko.Weep();
+
+            Cat maca = new Cat();
+            maca.Eat();
+            maca.Meow();
+        }
+    }
+}
