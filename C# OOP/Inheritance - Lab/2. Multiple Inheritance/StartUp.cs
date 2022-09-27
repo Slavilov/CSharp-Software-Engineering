81d93f9fe955a84b9910946e72ddea1e939e2cdb
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-27 23:47:28 +0300

    Add files via upload

---
+++ C# OOP/Inheritance - Lab/2. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy sharko = new Puppy();
+            sharko.Eat();
+            sharko.Bark();
+            sharko.Weep();
+        }
+    }
+}
