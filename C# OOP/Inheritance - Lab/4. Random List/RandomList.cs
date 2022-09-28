4ad3189b61abf2d38950bd7ac95e7ffc12de41bb
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-28 23:23:29 +0300

    Add files via upload

---
+++ C# OOP/Inheritance - Lab/4. Random List/RandomList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public void RemoveRandomElement()
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, this.Count-1);
+
+            Console.WriteLine($"Removing element at index -> {randomIndex} which is {this[randomIndex]}");
+            
+            this.RemoveAt(randomIndex);
+        }
+
+        public string RandomString()
+        {
+            string elementAtIndex = string.Empty;
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, this.Count-1);
+            return elementAtIndex = this[randomIndex];
+        }
+
+    }
+}
---
+++ C# OOP/Inheritance - Lab/4. Random List/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList nekuvList = new RandomList();
+            nekuvList.Add("ivanka");
+            nekuvList.Add("draganka");
+            nekuvList.Add("petkanka");
+            nekuvList.Add("ivancho");
+            nekuvList.Add("dragancho");
+            nekuvList.Add("petkancho");
+
+            Console.WriteLine($"Elements in the list before removing elements {nekuvList.Count}");
+            nekuvList.RemoveRandomElement();
+            Console.WriteLine($"Elements left in the list {nekuvList.Count}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Now giving random element:");
+            Console.WriteLine(nekuvList.RandomString());
+        }
+    }
+}
