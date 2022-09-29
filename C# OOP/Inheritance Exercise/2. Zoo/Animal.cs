4734069cea3503b26db1c4412554f8303bf791a3
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-09-29 23:53:01 +0300

    Add files via upload

---
+++ C# OOP/Inheritance Exercise/2. Zoo/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+    }
+}
