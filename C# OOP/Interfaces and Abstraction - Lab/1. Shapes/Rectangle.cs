13bf17821e3372a60cec0d071b52afc8a2cccc7d
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-10-08 23:34:13 +0300

    Add files via upload

---
+++ C# OOP/Interfaces and Abstraction - Lab/1. Shapes/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int heigth;
+
+        public Rectangle(int width, int heigth)
+        {
+            this.width = width;
+            this.heigth = heigth;
+        }
+
+        public void Draw()
+        {
+            DrawLine('*', '*');
+            for (int i = 0; i < heigth-2; i++)
+            {
+                DrawLine('*', ' ');
+            }
+            DrawLine('*', '*');
+        }
+
+        private void DrawLine(char boundary, char inner)
+        {
+            Console.WriteLine($"{boundary}{new string(inner, width -2)}{boundary}");
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Lab/1. Shapes/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Shapes
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(10);
+            circle.Draw();
+
+            Console.WriteLine();
+
+            Rectangle rectangle = new Rectangle(40, 15);
+            rectangle.Draw();
+        }
+    }
+}
