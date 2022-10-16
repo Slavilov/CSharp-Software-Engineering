843a13003dc7574944d93990b4923f5d1e32dae4
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-10-16 22:36:43 +0300

    Add files via upload

---
+++ C# OOP/Interfaces and Abstraction - Lab/2. Cars/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Cars
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            IElectricCar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat);
+            seat.Start();
+            seat.Stop();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(tesla);
+            tesla.Start();
+            tesla.Stop();
+
+        }
+    }
+}
---
+++ C# OOP/Interfaces and Abstraction - Lab/2. Cars/Tesla.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Batteries = battery;
+        }
+
+        public int Batteries { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Batteries} Batteries";
+
+        }
+    }
+}
