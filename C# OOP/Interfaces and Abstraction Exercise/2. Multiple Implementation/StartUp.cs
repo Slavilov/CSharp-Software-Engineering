601241407d74b24af8b9eab94cf290179aa30467
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-10-17 22:58:57 +0300

    Add files via upload

---
+++ C# OOP/Interfaces and Abstraction Exercise/2. Multiple Implementation/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PersonInfo
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+
+        }
+    }
+}
