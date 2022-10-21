75138b50f181a9b6db71069e04d4cca10aaebc7f
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-10-21 23:54:34 +0300

    Add files via upload

---
+++ C# OOP/Polymorphism Lab/1. MathOperation/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+
+        public int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public double Add(double one, double two, double three)
+        {
+            return one + two + three;
+        }
+
+        public decimal Add(decimal one, decimal two, decimal three)
+        {
+            return one + two + three;
+        }
+    }
+}
---
+++ C# OOP/Polymorphism Lab/1. MathOperation/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Operations
+
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations newMo = new MathOperations();
+            Console.WriteLine(newMo.Add(2,3));
+
+
+        }
+    }
+}
