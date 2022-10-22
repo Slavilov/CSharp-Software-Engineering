b33a00ebd6ba68f68d81aa14c0303163f3c2fcc0
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-10-22 23:26:59 +0300

    Add files via upload

---
+++ C# OOP/Polymorphism Lab/2. Animals/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Animals
+
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Peter", "Whiskas");
+            Animal dog = new Dog("George", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+        }
+    }
+}
